4af003460b89e9ef6a7b114878ab50987f043f70
tw93 <tw93@qq.com>
2015-04-22 22:40:24 +0800

    使用directive

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,15 +1,29 @@
 'use strict';
 //console.log(window.location());
 //console.log('\'Allo \'Allo! Popup');
-// angular.module('myApp',['ngRoute']).
-// config(['$routeProvider',function($routeProvider){
-//     $routeProvider
-//         .when('/',{
-//             templateUrl:'views/welcome.html',
-//             controller:'Welcome'
-//         })
-//         .when('/draw',{
-//             templateUrl:'views/welcome.html',
-//             controller:'Draw'
-//         })
-// }])
+angular.module('myApp',[])
+.directive('welcome',function(){
+    return {
+        restrict:'CA',
+        templateUrl:'views/welcome.html'
+    }
+}).directive('drawset',function(){
+    return {
+        restrict:'CA',
+        templateUrl:'views/drawset.html'
+    }
+}).directive('drawshow',function(){
+    return {
+        restrict:'CA',
+        templateUrl:'views/drawshow.html'
+    }
+}).controller('myController',['$scope',function($scope){
+    $scope.come=1;
+    $scope.set=0;
+    $scope.show=0;
+    $scope.toDrawSet=function(){
+        $scope.come=0;
+        $scope.set=1;
+    }
+    
+}])
