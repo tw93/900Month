845583751cdf816c41f534aaf25494ba1426b3d3
tw93 <tw93@qq.com>
2015-04-22 21:42:39 +0800

    不能使用angular-route

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,15 @@
 'use strict';
-
-console.log('\'Allo \'Allo! Popup');
+//console.log(window.location());
+//console.log('\'Allo \'Allo! Popup');
+// angular.module('myApp',['ngRoute']).
+// config(['$routeProvider',function($routeProvider){
+//     $routeProvider
+//         .when('/',{
+//             templateUrl:'views/welcome.html',
+//             controller:'Welcome'
+//         })
+//         .when('/draw',{
+//             templateUrl:'views/welcome.html',
+//             controller:'Draw'
+//         })
+// }])
