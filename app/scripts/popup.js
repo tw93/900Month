355e1505fe59bec07b36c61f365eb6b5e6e61699
tw93 <tw93@qq.com>
2015-04-24 11:15:52 +0800

    增加tableshow页面同时修改页面大小

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,41 +1,44 @@
 'use strict';
 //console.log(window.location());
 //console.log('\'Allo \'Allo! Popup');
-angular.module('myApp',['datePicker'])
-.directive('welcome',function(){
-    return {
-        restrict:'CA',
-        templateUrl:'views/welcome.html'
-    }
-}).directive('drawset',function(){
-    return {
-        restrict:'CA',
-        templateUrl:'views/drawset.html'
-    }
-}).directive('drawshow',function(){
-    return {
-        restrict:'CA',
-        scope:true,
-        templateUrl:'views/drawshow.html'
-    }
-}).controller('myController',['$scope',function($scope){
-    $scope.st=0;
-    $scope.isDrawbody=false;
-    $scope.toDrawSet=function(){
-        $scope.st=1;
-        $scope.isDrawbody=true;
-    };
-    $scope.returnWelcome=function(){
-        $scope.st=0;
-        $scope.isDrawbody=false;
-    };
-    $scope.toDrawShow=function(){
-        $scope.st=2;
-        $scope.isDrawbody=true;
-        $scope.isDrawbody=true;
-        $scope.monthSts=Array.apply(null,Array(900)).map(function(item,i){
-            return 0;
-        })
-    }
-    
-}])
+angular.module('myApp', ['datePicker'])
+    .directive('welcome', function() {
+        return {
+            restrict: 'CA',
+            templateUrl: 'views/welcome.html'
+        }
+    }).directive('drawset', function() {
+        return {
+            restrict: 'CA',
+            scope: true,
+            templateUrl: 'views/drawset.html'
+        }
+    }).directive('drawshow', function() {
+        return {
+            restrict: 'CA',
+            scope: true,
+            templateUrl: 'views/drawshow.html'
+        }
+    }).controller('myController', ['$scope', function($scope) {
+        $scope.st = 0;
+        $scope.isDrawbody = false;
+        $scope.now = new Date();
+        $scope.toDrawSet = function() {
+            $scope.st = 1;
+            $scope.isDrawbody = true;
+        };
+        $scope.returnWelcome = function() {
+            $scope.st = 0;
+            $scope.isDrawbody = false;
+        };
+        $scope.toDrawShow = function(time) {
+            console.log(time);
+            $scope.st = 2;
+            $scope.isDrawbody = true;
+            $scope.time = time;
+            $scope.monthSts = Array.apply(null, Array(900)).map(function(item, i) {
+                item = (i > 300) ? 0 : 1;
+                return item;
+            })
+        }
+    }])
