b6c57d894bc1b6acedf9100f3874f3fc2d2d4486
tw93 <tw93@qq.com>
2015-04-27 15:58:53 +0800

    remove the bad function

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,6 +1,4 @@
 'use strict';
-//console.log(window.location());
-//console.log('\'Allo \'Allo! Popup');
 angular.module('myApp', ['datePicker'])
     .directive('welcome', function() {
         return {
@@ -20,9 +18,10 @@ angular.module('myApp', ['datePicker'])
             templateUrl: 'views/drawshow.html'
         }
     }).controller('myController', ['$scope', function($scope) {
+        $scope.st = 0;
+        $scope.isDrawbody = false;
         $scope.now = new Date();
         chrome.storage.local.get('900MonthBirthday', function(result) {
-            console.log(result['900MonthBirthday']);
             var saveTime = result['900MonthBirthday'];
             if (saveTime) {
                 $scope.st = 2;
@@ -44,57 +43,48 @@ angular.module('myApp', ['datePicker'])
                         return 2;
                     }
                 });
-               
-            } else {
+            }
+            $scope.returnWelcome = function() {
                 $scope.st = 0;
                 $scope.isDrawbody = false;
+                chrome.storage.local.remove('900MonthBirthday', function() {
+                    console.log("remove!");
+                })
             }
-             $scope.remove = function() {
-                    $scope.st = 0;
-                    $scope.isDrawbody = false;
-                    chrome.storage.local.remove('900MonthBirthday', function() {
-                        console.log("remove!");
-                    })
-                }
             $scope.toDrawSet = function() {
-                    $scope.st = 1;
-                    $scope.must = false;
-                    $scope.isDrawbody = true;
-                };
-                $scope.returnWelcome = function() {
-                    $scope.st = 0;
-                    $scope.isDrawbody = false;
-                };
-                $scope.toDrawShow = function(time) {
-                    $scope.must = true;
-                    if ((time == undefined) || (time >= $scope.now)) {
-                        return false;
-                    }
-                    $scope.st = 2;
-                    $scope.isDrawbody = true;
-                    $scope.time = time;
-                    var dataObj = {};
-                    dataObj['900MonthBirthday'] = $scope.time.toString();
-                    chrome.storage.local.set(dataObj, function() {
-                        console.log('birthday has saved!');
-                    });
-                    var birYear = $scope.time.getFullYear(),
-                        birMonth = $scope.time.getMonth(),
-                        nowYear = $scope.now.getFullYear(),
-                        nowMonth = $scope.now.getMonth();
-                    $scope.haveMonth = 12 - birMonth + (nowYear - birYear) * 12 + nowMonth + 1;
-                    $scope.day = Math.floor(($scope.now - $scope.time) / 86400000);
-                    $scope.monthSts = Array.apply(null, Array(900)).map(function(item, i) {
-                        item = (i > $scope.haveMonth) ? 0 : 1;
-                        if (i < $scope.haveMonth) {
-                            return 0;
-                        } else if (i === $scope.haveMonth) {
-                            return 1;
-                        } else {
-                            return 2;
-                        }
-                    })
+                $scope.st = 1;
+                $scope.must = false;
+                $scope.isDrawbody = true;
+            };
+            $scope.toDrawShow = function(time) {
+                $scope.must = true;
+                if ((time == undefined) || (time >= $scope.now)) {
+                    return false;
                 }
+                $scope.st = 2;
+                $scope.isDrawbody = true;
+                $scope.time = time;
+                var dataObj = {};
+                dataObj['900MonthBirthday'] = $scope.time.toString();
+                chrome.storage.local.set(dataObj, function() {
+                    console.log('birthday has saved!');
+                });
+                var birYear = $scope.time.getFullYear(),
+                    birMonth = $scope.time.getMonth(),
+                    nowYear = $scope.now.getFullYear(),
+                    nowMonth = $scope.now.getMonth();
+                $scope.haveMonth = 12 - birMonth + (nowYear - birYear) * 12 + nowMonth + 1;
+                $scope.day = Math.floor(($scope.now - $scope.time) / 86400000);
+                $scope.monthSts = Array.apply(null, Array(900)).map(function(item, i) {
+                    item = (i > $scope.haveMonth) ? 0 : 1;
+                    if (i < $scope.haveMonth) {
+                        return 0;
+                    } else if (i === $scope.haveMonth) {
+                        return 1;
+                    } else {
+                        return 2;
+                    }
+                })
+            }
         });
-
     }])
